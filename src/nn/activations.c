1c44e12da4dd4c31505f02fb0d1f3f14a7623081
Arjun Basandrai <arjunbasandrai2004@gmail.com>
2023-12-05 00:13:55 +0530

    feat: added swish and leaky relu activation functions

--- src/nn/activations.c
+++ src/nn/activations.c
@@ -7,6 +7,14 @@ double sigmoid(double x) {
     return 1.0 / (1.0 + exp(-x));
 }
 
+double swish(double x) {
+    return x * sigmoid(x);
+}
+
 double relu(double x) {
     return max(0, x);
+}
+
+double leaky_relu(double x) {
+    return max(0.01 * x, x);
 }
--- src/nn/activations.h
+++ src/nn/activations.h
@@ -3,4 +3,6 @@
 #include "../matrix/matrix.h"
 
 double sigmoid(double x);
+double swish(double x);
 double relu(double x);
+double leaky_relu(double x);
